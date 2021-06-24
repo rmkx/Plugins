73aca9871db0deed8a1a04173773255dda7738d5
rmkx <50556482+rmkx@users.noreply.github.com>
2021-06-24 02:31:01 +0200

    Add files via upload

---
+++ SpotifyBackground/SpotifyBackground.plugin.js
@@ -0,0 +1,56 @@
+/**
+* @name         SpotifyBackground
+* @description  Sets the spotify song cover as user card background.
+* @donate       https://bit.ly/3fnzq1Z
+* @source       asdf
+* @author       rmkx
+* @invite       HnGWVQbQBv
+* @version      0.5
+*/
+
+const UserPopoutHeader = BdApi.findModule((m) => m?.default?.displayName === 'UserPopoutHeader');
+const UserPopoutInfo = BdApi.findModule((m) => m?.default?.displayName === 'UserPopoutInfo');
+
+
+const backgroundPatch = () => BdApi.Patcher.after('userPopoutHeader-patch', UserPopoutHeader, 'default', (that, args, value) => {
+    const [props] = args;
+    let activityBackground;
+    if (props.activity && props.activity.name === "Spotify") {
+        activityBackground = {
+            className: "activity-background",
+            style: {
+                position: "absolute",
+                left: 0,
+                right: 0,
+                bottom: 0,
+                filter: "brightness(0.4)",
+                background: "url(https://i.scdn.co/image/" + props.activity.assets.large_image.substring(8, props.activity.assets.large_image.length) + ") center/cover no-repeat",
+                zIndex: "0"
+            }
+        };
+        if(!props.user.bannerURL) { activityBackground.style.top = "60px" }
+        else { activityBackground.style.top = "120px" }
+        value.props.children.splice(1, 0, BdApi.React.createElement("div", activityBackground));
+    }
+	return value;
+});
+const headerPatch = () => BdApi.Patcher.after('userInfo-patch', UserPopoutInfo, 'default', (that, args, value) => {
+    const [props] = args;
+    value.props.style = { background: "transparent", borderBottom: "none" };
+    return value;
+});
+
+module.exports = class SpotifyBackground {
+    load() { }
+
+    start() {
+        backgroundPatch();
+        headerPatch();
+    }
+    stop() {
+        BdApi.Patcher.unpatchAll('userPopoutHeader-patch');
+        BdApi.Patcher.unpatchAll('userInfo-patch');
+    }
+
+    observer(changes) { }
+}
